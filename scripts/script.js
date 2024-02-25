12c0a7599d013489719c029b1c7f49263f14408e
Huan <98620169+bentonaw@users.noreply.github.com>
2024-02-25 21:06:16 +0100

    commit 1

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+// Cursor effects
+const blob = document.getElementById("blob");
+let lastMouseX = 0;
+let lastMouseY = 0;
+
+document.addEventListener("mousemove", function (e) {
+    var mouseX = e.clientX;
+    var mouseY = e.clientY;
+
+    // Check if mouseX or mouseY is null, if so, use the last recorded position
+    mouseX = mouseX || lastMouseX;
+    mouseY = mouseY || lastMouseY;
+
+    if (blob) {
+        blob.style.left = mouseX + "px";
+        blob.style.top = mouseY + "px";
+    }
+
+    // Update lastMouseX and lastMouseY
+    lastMouseX = mouseX;
+    lastMouseY = mouseY;
+})
