36dca7cd45aa787ebc051d6c4e7f47301ca4a64e
Huan <98620169+bentonaw@users.noreply.github.com>
2024-02-26 01:36:02 +0100

    added secrets

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,54 @@
 // Cursor effects
 const blob = document.getElementById("blob");
+const eegg = document.getElementById("eegg");
 let lastMouseX = 0;
 let lastMouseY = 0;
 
 document.addEventListener("mousemove", function (e) {
-    var mouseX = e.clientX;
-    var mouseY = e.clientY;
+	var mouseX = e.clientX;
+	var mouseY = e.clientY;
 
-    // Check if mouseX or mouseY is null, if so, use the last recorded position
-    mouseX = mouseX || lastMouseX;
-    mouseY = mouseY || lastMouseY;
+	// Check if mouseX or mouseY is null, if so, use the last recorded position
+	mouseX = mouseX || lastMouseX;
+	mouseY = mouseY || lastMouseY;
 
-    if (blob) {
-        blob.style.left = mouseX + "px";
-        blob.style.top = mouseY + "px";
-    }
+	if (blob) {
+		blob.style.left = mouseX + "px";
+		blob.style.top = mouseY + "px";
+	}
 
-    // Update lastMouseX and lastMouseY
-    lastMouseX = mouseX;
-    lastMouseY = mouseY;
-})
+	// Update lastMouseX and lastMouseY
+	lastMouseX = mouseX;
+	lastMouseY = mouseY;
+});
 
+if (window.location.href.includes("About.html")) {
+	eegg.addEventListener("click", function (e) {
+		blob.style.background =
+			"linear-gradient(to right, rgb(255, 255, 254), rgb(243, 159, 159))";
+	});
+}
 
+if (window.location.href.includes("Splash.html")) {
+	let pressedKeys = ""; // Initializing the variable
+
+	const messages = [
+		"Hax detected!",
+		"Unauthorized access!",
+		"Security breach!",
+		"Alert: suspicious activity detected!",
+		"Well, I’ve had enough nonsense. I’m going home!",
+		"It would be so nice if something made sense for a change.",
+	];
+
+	window.addEventListener("keyup", (e) => {
+		pressedKeys += e.key;
+		console.log(pressedKeys); // Check the value of pressedKeys in the console
+		pressedKeys = pressedKeys.slice(-4);
+		if (pressedKeys === "1337") {
+			const randomIndex = Math.floor(Math.random() * messages.length);
+			const randomMessage = messages[randomIndex];
+			alert(randomMessage);
+		}
+	});
+}
